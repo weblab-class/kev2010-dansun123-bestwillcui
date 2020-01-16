fcbbb934bffded22b49aa9e123b406de1528431d
dansun123 <41485135+dansun123@users.noreply.github.com>
2020-01-15 22:43:27 -0500

    completed basic functionality checklist

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -5,7 +5,7 @@ import "../../utilities.css";
 import "./Skeleton.css";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "89738695293-ocu6eiao6gaq5apf7rraqiqpt6tp9rqa.apps.googleusercontent.com";
 
 class Skeleton extends Component {
   constructor(props) {
@@ -45,13 +45,13 @@ class Skeleton extends Component {
         </ul>
         <h2> What you need to change</h2>
         <ul>
-          <li>Change the font in utilities.css</li>
-          <li>Change the Frontend CLIENT_ID for Google Auth (Skeleton.js)</li>
-          <li>Change the Server CLIENT_ID for Google Auth (auth.js)</li>
-          <li>Change the Database SRV for Atlas (server.js)</li>
-          <li>Change the Database Name for MongoDB (server.js)</li>
-          <li>Add a favicon to your website at the path client/dist/favicon.ico</li>
-          <li>Update website title in client/dist/index.html</li>
+          <li>Change the font in utilities.css DONE</li>
+          <li>Change the Frontend CLIENT_ID for Google Auth (Skeleton.js) DONE</li>
+          <li>Change the Server CLIENT_ID for Google Auth (auth.js) DONE</li>
+          <li>Change the Database SRV for Atlas (server.js) DONE</li>
+          <li>Change the Database Name for MongoDB (server.js) DONE</li>
+          <li>Add a favicon to your website at the path client/dist/favicon.ico DONE</li>
+          <li>Update website title in client/dist/index.html DONE</li>
         </ul>
       </>
     );
--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,7 @@ const socket = require("./server-socket");
 
 // create a new OAuth client used to verify google sign-in
 //    TODO: replace with your own CLIENT_ID
-const CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const CLIENT_ID = "89738695293-ocu6eiao6gaq5apf7rraqiqpt6tp9rqa.apps.googleusercontent.com";
 const client = new OAuth2Client(CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
