1d7a840748e5a944949c793824edac0ecab8baec
Kevin Jiang <cloudwiz24@gmail.com>
2020-01-28 15:02:59 -0500

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/weblab-class/kev2010-dansun123-bestwillcui"""
    This reverts commit 497948b1246dabaabec09f50878d68a98d62c5e6.

--- client/src/components/pages/CreateRoom.js
+++ client/src/components/pages/CreateRoom.js
@@ -1,65 +1,11 @@
-import React, { useState } from "react";
-import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
-
-
-export default function CreateRoom(props) {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-  
-    function validateForm() {
-      return title.length > 0
-    }
-  
-    function handleSubmit(event) {
-      props.createRoom(title, description)
-      event.preventDefault();
-    }
-
-    function cancelSubmit(event) {
-        setTitle("")
-        setDescription("")
-        props.cancelSubmit()
-        event.preventDefault()
-    }
-  
-    return (
-      <div className = "Full">
-      <div className="Login">  
-        <form onSubmit={handleSubmit}>
-        
-          {/* <div className = ""> */}
-          <div className="right">
-          <FormGroup controlId="email" bsSize="large" className="form-inline" >
-            <FormLabel>Room Name: &nbsp;&nbsp; &nbsp; &nbsp;  &nbsp; &nbsp;&nbsp; </FormLabel>
-            <FormControl
-              type="title"
-              value={title}
-              onChange={e => setTitle(e.target.value)}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large" className="form-inline">
-            <FormLabel>Description: &nbsp; &nbsp;</FormLabel>
-            <FormControl
-              value={description}
-              onChange={e => setDescription(e.target.value)}
-              type="description"
-            />
-          </FormGroup>
-          </div>
-          <br></br>
-          <Button block bsSize="large" disabled={!validateForm()} type="submit" className="submitbutton">
-            Create!
-          </Button>
-        </form>
-
-        <form onSubmit = {cancelSubmit}>
-          <Button block bsSize="large" type="submit" className="submitbutton">
-            Cancel
-          </Button>
-        </form>
-        
-      </div>
-      </div>
-    );
-  }
-
+<form onSubmit={this.handleProfileSubmit}>
+                <label>
+                Room Name
+                <input 
+                    type="text" 
+                    name="title"
+                    value={this.state.tempprofile} 
+                    onChange={this.handleProfileChange} />
+                </label>
+                <input type="submit" value="Create Room" />
+            </form>
